733f3c215832f6c8f6a180073d360d7e961c5f00
Gabriella RoseMarino <Gabriellarosemarino@gmail.com>
2021-05-05 23:41:41 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const mainDocument = document.querySelector("#main-content")
+  console.log(mainDocument
+    )
+  const grabForm = document.getElementById("create-task-form")
+  console.log(grabForm);
+
+  const myToDoList = document.getElementById("tasks")
+  console.log(myToDoList)
+
+  grabForm.addEventListener("submit", function(thisIsThing){
+    thisIsThing.preventDefault()
+    console.log(thisIsThing.target)
+
+    const imputBox = document.getElementById("new-task-description").value;
+  console.log(imputBox)
+
+    const listImput = document.createElement("li")
+    listImput.textContent = imputBox
+
+    myToDoList.appendChild(listImput)
+
+    grabForm.reset();
+  })
+
+  
+
+
 });
